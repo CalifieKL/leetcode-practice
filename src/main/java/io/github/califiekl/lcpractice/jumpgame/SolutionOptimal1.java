0ef47e331eddbf15fbcaff981dc86d58e496dae8
CalifieKL <califiekluo@gmail.com>
2024-05-05 12:03:36 -0400

    added one optimal solution

--- src/main/java/io/github/califiekl/lcpractice/jumpgame/SolutionOptimal1.java
+++ src/main/java/io/github/califiekl/lcpractice/jumpgame/SolutionOptimal1.java
@@ -1,8 +1,37 @@
 package io.github.califiekl.lcpractice.jumpgame;
 
 public class SolutionOptimal1 implements Solution {
+
+    private int[] maxReachables;
+
+    public SolutionOptimal1(){}
+    public SolutionOptimal1(int[] nums){
+        nullCheck(nums);
+        initializeMaxReachables(nums);
+    }
+
+    private void initializeMaxReachables(int[] nums){
+        nullCheck(nums);
+        maxReachables = new int[nums.length];
+        maxReachables[0]=nums[0];
+        for(int i=1;i<nums.length;++i)
+            maxReachables[i]=Math.max(i+nums[i], maxReachables[i-1]);
+    }
+
     @Override
     public int jump(int[] nums) {
-        return 0;
+        nullCheck(nums);
+        initializeMaxReachables(nums);
+
+        int jump = 0;
+        int currentPosition = 0;
+        while(currentPosition<nums.length-1){
+            jump++;
+            currentPosition = maxReachables[currentPosition];
+        }
+
+        return jump;
     }
+
+
 }
