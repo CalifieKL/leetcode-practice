b2a96197451842bcb60fa91db641dfb706d1ec0f
CalifieKL <califiekluo@gmail.com>
2024-05-05 12:00:35 -0400

    added test for optimal solution class

---
+++ src/test/java/io/github/califiekl/lcpractice/jumpgame/TestSolutionOptimal1.java
@@ -0,0 +1,8 @@
+package io.github.califiekl.lcpractice.jumpgame;
+
+public class TestSolutionOptimal1 extends TestSolution{
+    @Override
+    protected Solution getSolution() {
+        return new SolutionOptimal1();
+    }
+}
