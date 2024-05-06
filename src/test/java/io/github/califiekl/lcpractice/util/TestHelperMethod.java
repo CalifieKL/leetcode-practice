9c12c4a1d50ff925b3c9aceb9cfc0a49a19ab038
CalifieKL <califiekluo@gmail.com>
2024-05-06 00:45:08 -0400

    added test case for null check

--- src/test/java/io/github/califiekl/lcpractice/util/TestHelperMethod.java
+++ src/test/java/io/github/califiekl/lcpractice/util/TestHelperMethod.java
@@ -1,2 +1,31 @@
-package io.github.califiekl.lcpractice.util;public class TestHelperMethod {
+package io.github.califiekl.lcpractice.util;
+
+import org.junit.Test;
+
+import static org.junit.Assert.assertTrue;
+
+public class TestHelperMethod {
+
+    @Test
+    public void nullCheckShouldThrowExceptionIfInputIsNull(){
+        boolean exceptionThrown = false;
+        try{
+            HelperMethod.nullCheck(null, RuntimeException::new, "test error message");
+        } catch (Exception ex){
+            exceptionThrown = true;
+            assertTrue(ex instanceof RuntimeException);
+            assertTrue("test error message".equals(ex.getMessage()));
+        }
+        assertTrue(exceptionThrown);
+    }
+    @Test
+    public void nullCheckShouldDoNothingIfInputIsNotNull(){
+        boolean exceptionThrown = false;
+        try{
+            HelperMethod.nullCheck(new Object(), RuntimeException::new, "test error message");
+        } catch (Exception ex){
+            exceptionThrown = true;
+        }
+        assertTrue(!exceptionThrown);
+    }
 }
