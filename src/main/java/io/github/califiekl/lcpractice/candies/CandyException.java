9fc4aeba2cec1bb7d2e5937fc170a6fb933b18d9
CalifieKL <califiekluo@gmail.com>
2024-05-10 21:40:38 -0400

    changed approach; added exception for formality

---
+++ src/main/java/io/github/califiekl/lcpractice/candies/CandyException.java
@@ -0,0 +1,5 @@
+package io.github.califiekl.lcpractice.candies;
+
+public class CandyException extends RuntimeException{
+    public CandyException(String message){ super(message); }
+}
--- src/main/java/io/github/califiekl/lcpractice/candies/SolutionOriginal.java
+++ src/main/java/io/github/califiekl/lcpractice/candies/SolutionOriginal.java
@@ -1,45 +1,85 @@
 package io.github.califiekl.lcpractice.candies;
 
+import io.github.califiekl.lcpractice.util.HelperMethod;
+
+import java.util.ArrayList;
+import java.util.List;
+
 public class SolutionOriginal implements Solution{
 
     @Override
     public int candy(int[] ratings) {
-        int totalCandies=0;
-        int trendStart=0;
-        Trend prevTrend=null; Trend currTrend=null;
-        int trendLength;
-        for (int i=1;i<ratings.length;++i){
-            currTrend = Trend.getCurrentTrend(ratings[i],ratings[i-1]);
-            if(currTrend.isDifferentFrom(prevTrend)){
-                trendLength=i-trendStart;
-                if(Trend.Flat.equals(prevTrend)){
-                    totalCandies+=(trendLength-1);
-                    trendStart=i-1;
-                } else {
-                    totalCandies+=((trendLength+1)*trendLength/2);
-                    trendStart=i;
-                }
+        if(ratings.length==1) return 1;
+        if(ratings.length==2)
+            if(ratings[0]!=ratings[1]) return 3;
+            else return 2;
+
+        List<int[]> processedRatings = new ArrayList<>();
+        int prevTrend = getTrend(ratings[0], ratings[1]);
+        int currTrend = Integer.MIN_VALUE;
+        int trendStart = 1;
+        for(int i=1;i<ratings.length-1;++i){
+            currTrend = getTrend(ratings[i], ratings[i+1]);
+            if(currTrend!=prevTrend){
+                processedRatings.add(getInterval(i-trendStart, prevTrend));
+                trendStart = i+1;
             }
-            prevTrend=currTrend;
-        }
-        if(trendStart==ratings.length-1){
-            if(ratings[trendStart]>ratings[trendStart-1]) return totalCandies+2;
-            else return totalCandies+1;
+            else if(i==ratings.length-2){
+                processedRatings.add(getInterval(i-trendStart+1, prevTrend));
+            }
+            prevTrend = currTrend;
         }
 
-        return totalCandies+((ratings.length-trendStart)*(ratings.length-trendStart+1)/2);
+
+        int[] currInteval=processedRatings.get(0);
+        int[] nextInteval = null;
+        int totalCandies=currInteval[1]>=0?1:currInteval[0]+2;
+        for(int i=0;i<processedRatings.size()-1;++i){
+            currInteval = processedRatings.get(i);
+            nextInteval = processedRatings.get(i+1);
+            if(currInteval[0]==0){
+                if(currInteval[1]==0)
+                    totalCandies+=(nextInteval[1]>0?1:2);
+                else
+                    totalCandies+=(currInteval[1]>0?2:1);
+            } else {
+                totalCandies+=getIntervalCandies(currInteval);
+                totalCandies+=getExtremumCandies(currInteval, nextInteval);
+            }
+        }
+
+        int[] lastInteval=processedRatings.get(processedRatings.size()-1);
+        if(lastInteval[0]==0)
+            if(lastInteval[1]==0)
+                totalCandies+=(currTrend>0?1:2);
+            else
+                totalCandies+=(lastInteval[1]>0?2:1);
+        else totalCandies+=getIntervalCandies(lastInteval);
+
+        totalCandies+=currTrend>0?lastInteval[0]+2:1;
+
+        return totalCandies;
     }
-    public enum Trend{
-        Upward, Downward, Flat;
-        public static Trend getCurrentTrend(int prev, int curr){
-            if(prev<curr) return Upward;
-            else if(prev>curr) return Downward;
-            return Flat;
-        }
-        public boolean isDifferentFrom(Trend prevTrend){
-            if(prevTrend==null) return false;
-            return !this.equals(prevTrend);
-        }
 
+    private int getTrend(Integer prev, Integer curr){
+        if(prev<curr) return 1;
+        else if(prev>curr) return -1;
+        return 0;
+    }
+
+    private int[] getInterval(int length, int trend){
+        return new int[]{length, trend};
+    }
+
+    private int getIntervalCandies(int[] inteval){
+        if(inteval[1]==0) return inteval[0];
+        return (inteval[0]*(inteval[0]+3)/2);
+    }
+
+    private int getExtremumCandies(int[] currInteval, int[] nextInteval){
+        if(nextInteval[1]>currInteval[1]) return 1;
+        if(nextInteval[1]==0) return currInteval[0]+2;
+        if(currInteval[1]==0) return nextInteval[0]+2;
+        return Math.max(currInteval[0], nextInteval[0])+2;
     }
 }
