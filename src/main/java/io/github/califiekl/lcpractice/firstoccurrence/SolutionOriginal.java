36f4ac89312aa8903962a48508bcdaf8e2ec94e8
CalifieKL <califiekluo@gmail.com>
2024-05-12 21:36:09 -0400

    added brute force solution - first occurrence

--- src/main/java/io/github/califiekl/lcpractice/firstoccurrence/SolutionOriginal.java
+++ src/main/java/io/github/califiekl/lcpractice/firstoccurrence/SolutionOriginal.java
@@ -3,6 +3,15 @@ package io.github.califiekl.lcpractice.firstoccurrence;
 public class SolutionOriginal implements Solution{
     @Override
     public int strStr(String haystack, String needle) {
-        return 0;
+        int needleLength = needle.length();
+        int haystackLength = haystack.length();
+        for(int i=0; i<haystackLength;++i){
+            if(i+needleLength>haystackLength) return -1;
+            for(int j=0;j<needleLength;++j){
+                if(haystack.charAt(i+j)!=needle.charAt(j)) break;
+                else if(j==needleLength-1) return i;
+            }
+        }
+        return -1;
     }
 }
