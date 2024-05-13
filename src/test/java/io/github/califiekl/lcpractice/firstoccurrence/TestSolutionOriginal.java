499c4a17a009dd53e2cd0961617105f6b92311e8
CalifieKL <califiekluo@gmail.com>
2024-05-12 21:10:12 -0400

    finished concrete test class - first occurrence

--- src/test/java/io/github/califiekl/lcpractice/firstoccurrence/TestSolutionOriginal.java
+++ src/test/java/io/github/califiekl/lcpractice/firstoccurrence/TestSolutionOriginal.java
@@ -1,4 +1,8 @@
 package io.github.califiekl.lcpractice.firstoccurrence;
 
 public class TestSolutionOriginal extends TestSolution{
+    @Override
+    protected Solution getSolution() {
+        return new SolutionOriginal();
+    }
 }
