c3f050b10b1b8727d835a66f48a13f075390716a
CalifieKL <califiekluo@gmail.com>
2024-05-16 00:30:40 -0400

    solved some basic aggregate problems

--- src/main/resources/sql/sql_practice.sql
+++ src/main/resources/sql/sql_practice.sql
@@ -81,3 +81,21 @@ select Signups.user_id as user_id, case when Rate.rate is null then 0 else Rate.
 select Signups.user_id, round(avg(case when Confirmations.action='confirmed' then 1 else 0 end), 2) as confirmation_rate from (Signups left outer join Confirmations on Signups.user_id = Confirmations.user_id)
 group by Signups.user_id
 --Note: avg() - confirmation_rate = #confirmation/#request=(1*#confirmation+0*#timeout)/(#confirmation+#timeout)
+
+--Average Selling Price
+--Solution Original
+select product_id,
+case when round(sum(units*price)/sum(units),2) is null then 0 else round(sum(units*price)/sum(units),2) end as average_price from (
+    select p.product_id as product_id, p.price as price, u.units as units
+    from Prices p left outer join UnitsSold u
+    on p.product_id=u.product_id and (u.purchase_date between p.start_date and p.end_date)
+)
+group by product_id;
+--Solution Cleaner (use coalesce)
+select product_id,
+coalesce(round(sum(units*price)/sum(units),2),0) as average_price from (
+    select p.product_id as product_id, p.price as price, u.units as units
+    from Prices p left outer join UnitsSold u
+    on p.product_id=u.product_id and (u.purchase_date between p.start_date and p.end_date)
+)
+group by product_id;
