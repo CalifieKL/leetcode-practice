4fef4366dbd44a63361d8c1ab7f28d0d038f79a8
CalifieKL <califiekluo@gmail.com>
2024-05-24 01:11:40 -0400

    added cleaner soluion to friend requests ii

--- src/main/resources/sql/sql_practice.sql
+++ src/main/resources/sql/sql_practice.sql
@@ -325,3 +325,15 @@ select id, num from(
         on r.id=a.id
     order by num desc
 ) where rownum=1;
+--Solution cleaner
+select id, num from (
+    select id, count(*) as num
+    from (
+        select requester_id as id from RequestAccepted
+        union all
+        select accepter_id as id from RequestAccepted
+        ) group by id
+        order by num desc
+    )
+where rownum=1;
+--Note: union all to preserve duplicates
