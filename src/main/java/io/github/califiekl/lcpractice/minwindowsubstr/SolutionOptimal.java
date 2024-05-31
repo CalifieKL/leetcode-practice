ba4d42833b5afc751f148e92a545b57a3f5561f7
CalifieKL <califiekluo@gmail.com>
2024-05-30 23:27:13 -0400

    added classes for optimal solution of minimum window substring

---
+++ src/main/java/io/github/califiekl/lcpractice/minwindowsubstr/SolutionOptimal.java
@@ -0,0 +1,8 @@
+package io.github.califiekl.lcpractice.minwindowsubstr;
+
+public class SolutionOptimal implements Solution{
+    @Override
+    public String minWindow(String s, String t) {
+        return null;
+    }
+}
---
+++ src/test/java/io/github/califiekl/lcpractice/minwindowsubstr/TestSolutionOptimal.java
@@ -0,0 +1,8 @@
+package io.github.califiekl.lcpractice.minwindowsubstr;
+
+public class TestSolutionOptimal extends TestSolution{
+    @Override
+    protected Solution getSolution() {
+        return new SolutionOptimal();
+    }
+}
