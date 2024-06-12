5498d8f6f3a1c2166e4f7459004455f835dc3259
CalifieKL <califiekluo@gmail.com>
2024-06-12 18:31:25 -0400

    added solution for rearrange product table

--- src/main/resources/sql/sql_practice.sql
+++ src/main/resources/sql/sql_practice.sql
@@ -524,3 +524,11 @@ group by t.request_at;
 select player_id, to_char(min(event_date),'yyyy-mm-dd') as first_login
 from Activity
 group by player_id;
+
+--Rearrange Product Table
+--Solution Collected
+select product_id, 'store1' as "store", store1 as "price" from Products where store1 is not null
+union
+select product_id, 'store2' as "store", store2 as "price" from Products where store2 is not null
+union
+select product_id, 'store3' as "store", store3 as "price" from Products where store3 is not null
