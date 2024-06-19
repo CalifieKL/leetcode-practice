9411e642fbe87d3e35269a6df96ef285a523c119
CalifieKL <califiekluo@gmail.com>
2024-06-18 22:24:46 -0400

    solved employees with missing information

--- src/main/resources/sql/sql_practice.sql
+++ src/main/resources/sql/sql_practice.sql
@@ -588,3 +588,9 @@ from (select account, sum(amount) as balance from Transactions group by account)
 left join Users u
 on u.account = a.account
 where a.balance>10000;
+
+--Employees With Missing Information
+select nvl(e.employee_id,s.employee_id) as employee_id from
+Employees e full outer join Salaries s on e.employee_id=s.employee_id
+where e.name is null or s.salary is null
+order by employee_id;
